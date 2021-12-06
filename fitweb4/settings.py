425eb4ccb219acb75369c877f89fcae9e35ba2af
tanya <grace.08de98@gmail.com>
2021-12-06 19:28:32 +0900

    13

--- fitweb4/settings.py
+++ fitweb4/settings.py
@@ -25,8 +25,7 @@ SECRET_KEY = 'django-insecure-bi4%3uwfnjfk0c%lq!+&73=rllc5d_pu&_614)zur-=vrbf#18
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
-
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', '[::1]', '.pythonanywhere.com']
 
 # Application definition
 
